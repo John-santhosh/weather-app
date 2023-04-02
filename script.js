f6d147c41cf7fc66090362abb159dd42351f2f75
john <johnsantosh2@gmail.com>
2023-04-02 14:01:23 +0530

    added parameter(units=metrics) to get the weather in celsius and changed to an new  api key

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "3ab79bf576mshb309e1d23f30744p148662jsnf618835f6fdc",
-    "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
-  },
-};
-
 // events for keyboard and search button
 $("button").click(fetchApi);
 $(document).keydown((e) => {
@@ -23,7 +15,7 @@ async function fetchApi() {
   }
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=9701e05bdc450703a5af1448835dd149`
+      `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=d4757c7636a66bec48e7538e1249bba2&units=metric`
     );
     const data = await response.json();
     //from displayTemperature function
@@ -33,15 +25,14 @@ async function fetchApi() {
     console.log(err);
   }
 }
-
 // filling up temperature details from the fetchApi()
 function displayTemperature(obj) {
-  console.log(obj);
+  // console.log(obj);
   const weatherImg = `https://openweathermap.org/img/w/${obj.weather[0].icon}.png`;
   $("#city-name").text(obj.name);
   $("#weather-type").text(obj.weather[0].main);
   $("#weather-icon").attr("src", weatherImg);
-  $("#temp").text((obj.main.temp - 273.15).toFixed(2));
-  $("#min-temp").text((obj.main.temp_max - 273.15).toFixed(2));
-  $("#max-temp").text((obj.main.temp_min - 273.15).toFixed(2));
+  $("#temp").text(obj.main.temp);
+  $("#min-temp").text(obj.main.temp_max);
+  $("#max-temp").text(obj.main.temp_min);
 }
